76425c37eb2c82b8ab0722d36849d3b0e9dc5180
tchandr1 <tchandr1@hawk.iit.edu>
2015-11-04 09:02:16 -0600

    mp1 updated

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+echo "SSH in ubuntu" 
+ssh ubuntu@ec2-52-26-56-48.us-west-2.compute.amazonaws.com -i ./itmo544-444-fall2015-surface-laptop.pem
+
 sudo apt-get update -y
-sudo apt-get install -y apache2 git
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
-git clone https://github.com/tchandr1/EnvironmentalSetup-itmo-544-444-fall2015.git
+curl -sS https://getcomposer.org/installer | php
 
-mv ./EnvironmentalSetup-itmo-544-444-fall2015/images /var/www/html/images
-mv ./EnvironmentalSetup-itmo-544-444-fall2015/index.html /var/www/html/
-
-echo "Hello! I am Thanusha Chandrahasa. Course:ITMO544-444, Week05Assignment" > /tmp/hello.txt
+sudo php composer.phar require aws/aws-sdk-php
 
 
+git clone https://github.com/tchandr1/EnvironmentalSetup-itmo-544-444-fall2015.git  
+
+echo "Moverequired files to var/www/html"
+    
+sudo mv ./EnvironmentalSetup-itmo-544-444-fall2015/images /var/www/html/images
+sudo mv ./EnvironmentalSetup-itmo-544-444-fall2015/index.html /var/www/html/
+sudo mv ./EnvironmentalSetup-itmo-544-444-fall2015/*.php /var/www/html/
+
+sudo mv vendor /var/www/html
+
+sudo php /var/www/html/setup.php
+
+echo "Hello! I am Thanusha Chandrahasa. Course:ITMO544-444, MiniProject1"
 
 
 
