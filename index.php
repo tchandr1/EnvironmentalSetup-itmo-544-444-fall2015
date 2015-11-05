af3fa2ed9391994d7cc7e8ad97b0372dcc7001cc
tchandr1 <tchandr1@hawk.iit.edu>
2015-11-05 09:41:10 -0600

    changed action of the form

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <body>
 
 <!-- The data encoding type, enctype, MUST be specified as below -->
-<form enctype="multipart/form-data" action="result.php" method="POST">
+<form enctype="multipart/form-data" action="submit.php" method="POST">
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
