5520da547be5a454d970814b3ded75d9ee2b0e57
tchandr1 <tchandr1@hawk.iit.edu>
2015-11-06 12:23:56 -0600

    changed db name and password

--- setup.php
+++ setup.php
@@ -14,17 +14,17 @@ $result = $rds->createDBInstance([
    # 'CopyTagsToSnapshot' => true || false,
    # 'DBClusterIdentifier' => '<string>',
     'DBInstanceClass' => 'db.t1.micro', // REQUIRED
-    'DBInstanceIdentifier' => 'itmo544tchdb', // REQUIRED
+    'DBInstanceIdentifier' => 'tch-db', // REQUIRED
     'DBName' => 'customerrecords',
     #'DBParameterGroupName' => '<string>',
     #'DBSecurityGroups' => ['<string>', ...],
     #'DBSubnetGroupName' => '<string>',
     'Engine' => 'MySQL', // REQUIRED
-    'EngineVersion' => '5.5.41',
+   # 'EngineVersion' => '5.5.41',
     #'Iops' => <integer>,
     #'KmsKeyId' => '<string>',
    # 'LicenseModel' => '<string>',
-  'MasterUserPassword' => 'letmein888',
+  'MasterUserPassword' => 'ilovebunnies',
     'MasterUsername' => 'controller',
     #'MultiAZ' => true || false,
     #'OptionGroupName' => '<string>',
@@ -47,24 +47,29 @@ $result = $rds->createDBInstance([
 ]);
 print "Create RDS DB results: \n";
 # print_r($rds);
-$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'itmo544tchdb',
+$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'tch-db',
 ]);
 // Create a table 
 $result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'itmo544tchdb',
+    'DBInstanceIdentifier' => 'tch-db',
 ]);
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
-$link = mysqli_connect($endpoint,"controller","letmein888","3306") or die("Error " . mysqli_error($link));
-echo "Here is the result: " . $link;
-$sql = "CREATE TABLE comments 
+$link = mysqli_connect($endpoint,"controller","letmein888","customerrecords") or die("Error " . mysqli_error($link));
+#echo "Here is the result: " . $link;
+$sql = "CREATE TABLE IF NOT EXISTS items  
 (
-ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500)
+    id INT NOT NULL AUTO_INCREMENT,
+    uname VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    s3rawurl VARCHAR(256) NOT NULL,
+    s3finishedurl VARCHAR(256) NOT NULL,
+    jpgfilename VARCHAR(255) NOT NULL,
+    state TINYINT(3) NOT NULL,
+    PRIMARY KEY(id)
 )";
-$con->query($sql);
+$link->query($sql);
 ?>
 
 
