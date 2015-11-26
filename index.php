f646699f62d35c0bd41bc68cc1af54c7514e7474
tchandr1 <tchandr1@hawk.iit.edu>
2015-11-26 09:54:02 -0600

    added Bootstrap

--- index.php
+++ index.php
@@ -1,30 +1,34 @@
 <?php session_start();?>
 <html>
 <head><title>Hello app</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
 </head>
 <body>
-
+<div class="container">
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="submit.php" method="POST">
+    <div class="form-group">
+    <label for="exampleInputEmail1">File Upload</label>
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-    Send this file: <input name="userfile" type="file" /><br />
-Enter Email of user: <input type="email" name="useremail"><br />
-Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
-
-
-<input type="submit" value="Send File" />
+    <label for="exampleInputEmail1">Send this file</label>    
+    <input name="userfile" type="file" /><br />
+    <label for="exampleInputEmail1">Enter Email of user </label>
+    <input type="email" name="useremail"><br />
+    <label for="exampleInputEmail1">Enter Phone of user (1-XXX-XXX-XXXX)</label>
+    <input type="phone" name="phone">
+    <input type="submit" class="btn btn-default" value="Send File" />
 </form>
 <hr />
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="gallery.php" method="POST">
 
 Enter Email of user for gallery to browse: <input type="email" name="email">
-<input type="submit" value="Load Gallery" />
+<input type="submit" class="btn btn-default" value="Load Gallery" />
 </form>
 
-
+</div>
 </body>
 </html>
 
