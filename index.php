b8ebe590fed30e79714fe07bcf93766a75ba27b8
tchandr1 <tchandr1@hawk.iit.edu>
2015-11-26 10:35:00 -0600

    added color buttons through bootstrap

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 </head>
 <body>
 <div class="container">
+<h1>Page to upload files</h1>
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="submit.php" method="POST">
     <div class="form-group">
@@ -12,20 +13,20 @@
     <!-- MAX_FILE_SIZE must precede the file input field -->
     <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
     <!-- Name of input element determines name in $_FILES array -->
-    <label for="exampleInputEmail1">Send this file</label>    
-    <input name="userfile" type="file" /><br />
+    <!--<label for="exampleInputEmail1">Send this file</label>-->    
+    <input name="userfile" class="btn btn-primary" type="file" /><br />
     <label for="exampleInputEmail1">Enter Email of user </label>
     <input type="email" name="useremail"><br />
     <label for="exampleInputEmail1">Enter Phone of user (1-XXX-XXX-XXXX)</label>
     <input type="phone" name="phone">
-    <input type="submit" class="btn btn-default" value="Send File" />
+    <input type="submit" class="btn btn-primary" value="Send File" />
 </form>
 <hr />
 <!-- The data encoding type, enctype, MUST be specified as below -->
 <form enctype="multipart/form-data" action="gallery.php" method="POST">
-
-Enter Email of user for gallery to browse: <input type="email" name="email">
-<input type="submit" class="btn btn-default" value="Load Gallery" />
+<label for="exampleInputEmail1">Enter Email of user for gallery to browse:</label>
+ <input type="email" name="email">
+<input type="submit" class="btn btn-primary" value="Load Gallery" />
 </form>
 
 </div>
