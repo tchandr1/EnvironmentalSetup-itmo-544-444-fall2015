98172dffbbf8328c4d7d938640b977a416e0c075
tchandr1 <tchandr1@hawk.iit.edu>
2015-11-28 11:36:21 -0600

    added restart stmt

--- install-webserver.sh
+++ install-webserver.sh
@@ -6,8 +6,9 @@ sudo apt-get update -y
 sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 sudo apt-get install php5 php5-imagick
 sleep 10
+sudo service apache2 restart
 sudo apt-get apache2 reload
-sleep 10
+sleep 5
 
 git clone https://github.com/tchandr1/EnvironmentalSetup-itmo-544-444-fall2015.git  
 
